7db788b6015f2edfd7347300a0ea43c7cff32303
DenysHnaatiuk <denys.hnatiuk@gmail.com>
2017-07-18 15:29:16 +0300

    initial commit

---
+++ template-parts/contacts.php
@@ -0,0 +1,54 @@
+      <div id="contacts" class="section contacts container-fluid">
+        <h2 class="section_header">Contacts</h2>
+        <form role="form" class="col-xs-12 col-md-6">
+          <br style="clear:both">
+          <h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+          <div class="form-group">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+          </div>
+          <div class="form-group">
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+          </div>
+          <button type="button" id="submit" name="submit" class="btn btn-block btn-success">Submit Form</button>
+        </form>
+        <div class="col-xs-12 col-md-6">
+            <table class="table">
+          <tbody>
+            <tr>
+              <a href="#" class="manager">
+                <td>manager</td>
+                <td>John Smith</td>
+              </a>
+            </tr>
+            <tr>
+              <a href="#" class="call">
+              <td>Tel</td>
+              <td>+38 (000) 000 00 00</td>
+              </a>
+            </tr>
+            <tr>
+              <a href="#" class="mailto">
+              <td>Е-mail</td>
+              <td>mailbox@domain</td>
+              </a>
+            </tr>
+            <tr>
+              <a href="#" class="map">
+              <td>Adress</td>
+              <td>Ukraine, Mikolayivska obl., Mykolayiv, Shneerson str. </td>
+              </a>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
